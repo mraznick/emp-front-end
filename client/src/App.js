d6aa1d0ed7a596c50c6a590cb6b0f1644d10d210
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-07-29 09:48:34 -0400

    built out App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div>Here we go</div>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pictures" element={<Pictures />} />
+        <Route path="/pictures/:id" element={<PictureDetail />} />
+        {/* <Route path="/add-comment" element={<AddComment />} />
+        <Route path="/pictures/:id/edit" element={<PictureEdit />} /> */}
+        {/* Elements/Screens aren't made yet;
+        unsure if we want to create paths for adding a comment/editing a picture */}
+      </Routes>
     </div>
   );
 }
